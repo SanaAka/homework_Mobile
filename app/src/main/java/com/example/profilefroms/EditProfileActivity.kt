079a49bc6e2573fd1f38a7d2b92e875d1dac50d9
Khem Phalveasana <veasnakh0963080326@gmail.com>
2025-06-06 23:49:23 +0700

    Added project files

---
+++ app/src/main/java/com/example/profilefroms/EditProfileActivity.kt
@@ -0,0 +1,51 @@
+package com.example.profilefroms
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.profilefroms.databinding.ActivityEditProfileBinding
+
+class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditProfileBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Get fullName from profile
+        val firstName = intent.getStringExtra(MainActivity.FIRST_NAME)
+        val userName = intent.getStringExtra(MainActivity.USER_NAME)
+        val generation = intent.getStringExtra(MainActivity.GENERATION)
+
+        //Set fullName to EditText
+        binding.editFirstname.setText(firstName)
+        binding.editUsername.setText(userName)
+        binding.editGeneration.setText(generation)
+
+        //Close activity
+        binding.closeBtn.setOnClickListener{
+            finish()
+        }
+
+        //Save new username
+        binding.saveBtn.setOnClickListener{
+            editProfile()
+        }
+    }
+    private fun editProfile(){
+        val newFirstName = binding.editFirstname.text.toString()
+        val newUserName = binding.editUsername.text.toString()
+        val newGeneration = binding.editGeneration.text.toString()
+
+        val dataIntent = Intent()
+        dataIntent.putExtra(MainActivity.FIRST_NAME, newFirstName)
+        dataIntent.putExtra(MainActivity.USER_NAME, newUserName)
+        dataIntent.putExtra(MainActivity.GENERATION, newGeneration)
+        setResult(RESULT_OK, dataIntent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/profilefroms/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.profilefroms
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.profilefroms.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    //register for activity new username
+    private val activityLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+
+        if(it.resultCode != RESULT_OK){
+            return@registerForActivityResult
+        }
+        val newFirstName = it.data?.getStringExtra(FIRST_NAME)
+        val newUserName = it.data?.getStringExtra(USER_NAME)
+        val newGeneration = it.data?.getStringExtra(GENERATION)
+
+        binding.firstName.text = "$newFirstName"
+        binding.profileName.text = "$newUserName"
+        binding.Generation.text = "$newGeneration"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editIc.setOnClickListener {
+            goToEditProfile()
+        }
+    }
+
+    private fun goToEditProfile() {
+        val intent = Intent(this, EditProfileActivity::class.java)
+
+        val firstName = binding.firstName.text.toString()
+        val userName = binding.profileName.text.toString()
+        val generation = binding.Generation.text.toString()
+
+        intent.putExtra(FIRST_NAME, firstName)
+        intent.putExtra(USER_NAME, userName)
+        intent.putExtra(GENERATION, generation)
+
+        activityLauncher.launch(intent)
+    }
+
+    companion object{
+        const val FIRST_NAME = "firstName"
+        const val USER_NAME = "userName"
+        const val GENERATION = "generation"
+    }
+
+}
+
+
